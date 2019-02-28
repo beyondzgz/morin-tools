0c69c88ba4ab50ef8b2b04e9a6b23caeef303e4f
Morin <18500233360@qq.com>
2019-02-28 16:51:58 +0800

    t

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require_once './vendor/autoload.php';
+use morin\php\Tools;
+
+// Tools::href('http://baidu.com');
+
+// Tools::json();
+
+// Tools::logs();
+
+// echo Tools::gukey();
+
+// echo Tools::guid();
+
+// echo Tools::order_num();
+
+// Tools::curl();
+
+// echo Tools::passed_time(time() - 60);
+
+// echo Tools::decrypt();
+
+// var_dump(Tools::build_tree());
+
+var_dump(Tools::week2cn());
---
+++ run.bat
@@ -0,0 +1,2 @@
+D:\xampp\php\php.exe -S 0.0.0.0:2222 -t D:\xampp\htdocs\morin-tools
+pause
